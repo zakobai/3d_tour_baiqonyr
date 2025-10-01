4f80d0ac959fc42d75d1d7328e1ecbff137b1be7
zakobai <zhan.aman96@gmail.com>
2025-10-01 16:14:58 +0500

    first test

---
+++ data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_6459-2",
+      "name": "IMG_6459-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1968,
+      "initialViewParameters": {
+        "yaw": -0.11157249681988368,
+        "pitch": 0.2773528221168391,
+        "fov": 1.3936083475958063
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
